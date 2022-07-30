4be97611816bb1fc094e5805cf0e4e23d7648385
Prasad Jivane <prasadjivane@gmail.com>
2022-07-30 23:10:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,16 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
 from fastapi import FastAPI
 import uvicorn
 
 app = FastAPI()
 
+@app.get("/")
+async def root():
+    return {"message": "Hello Prasad"}
 
-@app.get('/')
-def root():
-    return {'message': 'Hello Prasad'}
-
-
-@app.get('/test')
-def test():
-    return {'message': 'Test Worked'}
-
-
-@app.get('/add/str1')
-def add(str1):
+@app.get("/add/str1")
+async def add(str1):
     en = str1
-    return {'Entered Value': en}
+    return {"Entered Value": en}
 
 if __name__ == '__main__':
     uvicorn.run(app, port=8080, host='0.0.0.0')
